d8dfa3f7619bfc8498c58b53e9deb25f14103973
ziyadshaikh013 <ziyadshaikh013@gmail.com>
2020-02-12 15:01:25 +0530

    final commite

--- script.js
+++ script.js
@@ -7,4 +7,27 @@ var tl = new TimelineMax();
     .fromTo('#maittle', 1, {top: '100%'}, { top: '62%', ease: Expo.easeInOut}, '-=1')
     .fromTo('#maittle-bottom', 1, {top: '-30%'}, { top: '-84px', ease: Expo.easeInOut}, '-=1')
     .fromTo('#myimg', 1, {opacity:0, transform: 'scale(1.1)'}, { opacity: 1, transform: 'scale(1)', ease: Expo.easeInOut}, '-=1')
-    .fromTo('#play', 2, {opacity: 0}, { opacity: 1, ease: Expo.easeInOut})
+    .fromTo('#play', 2, {opacity: 0}, { opacity: 1, ease: Expo.easeInOut});
+
+
+var play = document.querySelector('#play');
+
+play.addEventListener('click', function(){
+   var tl2 = new TimelineMax();
+   tl2.fromTo('#layer1', 1 , {left: '100%'}, {left: 0, ease: Expo.easeInOut})
+      .fromTo('#leftline', 1, {height: 0}, {height: '35%'})
+      .fromTo('#rightbar', 1, {width: 0}, {width: '15%'}, '-=1')
+      .fromTo('#modelimage img', 1, {bottom: '100%'}, {bottom: 0, ease: Expo.easeIn}, '-=1')
+      .fromTo('#textdets', 1, {opacity: 0, left: '20%'}, {left: '42%', opacity: 1, ease: Expo.easeIn});
+
+});
+
+document.querySelector('#naigateback i, p').addEventListener('click', function(){
+   var tl3 = new TimelineMax();
+   tl3.fromTo('#layer1', 1 , {left: 0}, {left: '100%', ease: Expo.easeInOut});
+})
+document.querySelector('#naigateback p').addEventListener('click', function(){
+   var tl4 = new TimelineMax();
+   tl4.fromTo('#layer1', 1 , {left: 0}, {left: '100%', ease: Expo.easeInOut});
+})
+  
